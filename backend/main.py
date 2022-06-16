b70d542851a259916bd822e88bdf2b645c389bf4
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-16 10:29:01 -0700

    Add routes

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Reminder(BaseModel):
+    text: str
+
+reminders = []
+
+@app.get("/")
+def read_root():
+    return "Welcome!"
+
+@app.get("/get-reminders")
+def get_reminders():
+    return reminders
+
+@app.put("/add-reminder")
+def add_reminder(reminder: Reminder):
+    reminders.append(reminder)
